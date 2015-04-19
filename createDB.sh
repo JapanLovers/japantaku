9a7e87816a285818cb9268be4645bfa959237fd4
Vincent ALBERT <albert.vincent@hotmail.fr>
2015-04-19 16:21:22 +0200

    Creating a new repo

--- createDB.sh
+++ createDB.sh
@@ -12,12 +12,12 @@ rails d model comment
 echo 'DOING rails d model article'
 rails d model article
 
-echo 'DOING rails generate model Comment content:text pseudo:string'
-rails generate model Comment content:text pseudo:string
-echo 'DOING rails generate model Article title:string:uniq date:datetime content:string'
-rails generate model Article title:string:uniq date:datetime content:string
-echo 'DOING rails generate model User email:string:uniq pseudo:string:uniq desc:string age:integer avatar:string last_name:string first_name:string pwd:string admin:boolean'
+echo 'DOING rails generate model User email:string:uniq pseudo:string:uniq desc:text age:integer avatar_name:string last_name:string first_name:string pwd:string isAdmin:boolean'
 rails generate model User email:string:uniq pseudo:string:uniq desc:string age:integer avatar:string last_name:string first_name:string pwd:string admin:boolean
+echo 'DOING rails generate model Article title:string:uniq date:datetime content:text user:reference'
+rails generate model Article title:string:uniq date:datetime content:string
+echo 'DOING rails generate model Comment content:text pseudo:string comment:references article:references'
+rails generate model Comment content:text pseudo:string
 
 echo 'DOING rake db:migrate'
 rake db:migrate
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419135647) do
+ActiveRecord::Schema.define(version: 20150419141525) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
