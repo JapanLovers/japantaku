36d70b7659fc96d6fd426a26981b6fb0c0e05a3f
Adonis Najimi <adopi.naj@gmail.com>
2015-04-19 19:08:56 +0200

    Update createDB.sh

--- createDB.sh
+++ createDB.sh
@@ -12,7 +12,7 @@ rails d model comment
 echo 'DOING rails d model article'
 rails d model article
 
-echo 'DOING rails generate model user email:string:uniq pseudo:string:uniq desc:text age:integer avatar_name:string last_name:string first_name:string pwd:string isAdmin:boolean'
+echo 'DOING rails generate model user email:email:uniq pseudo:string:uniq desc:text age:integer avatar_name:string last_name:string first_name:string pwd:string isAdmin:boolean'
 rails generate model User email:string:uniq pseudo:string:uniq desc:text age:integer avatar_name:string last_name:string first_name:string pwd:string isAdmin:boolean
 echo 'DOING rails generate model article title:string:uniq content:text user:references'
 rails generate model Article title:string:uniq content:text user:references
